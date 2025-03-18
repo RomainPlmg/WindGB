64938100b3bc22f303286ed73c2bca3bfcf67d33
Romain Plumaugat <romain.plumaugat@protonmail.com>
2025-03-18 23:43:07 +0100

    Starting implementing opcode table

---
+++ src/core/opcodes.cpp
@@ -0,0 +1,34 @@
+#include "opcodes.h"
+
+// TODO: Implement function body
+void Op_Nop(CPU& cpu) { return; }  // Nop
+
+void Op_LD_BC_n16(CPU& cpu) {}  // LD BC, n16
+
+void Op_LD_DE_n16(CPU& cpu) {}  // LD DE, n16
+
+void Op_LD_HL_n16(CPU& cpu) {}  // LD HL, n16
+
+void Op_LD_SP_n16(CPU& cpu) {}  // LD SP, n16
+
+void Op_LD_BC_A(CPU& cpu) {}  // LD [BC], A
+
+void Op_LD_DE_A(CPU& cpu) {}  // LD [DE], A
+
+void Op_LD_HL_plus_A(CPU& cpu) {}  // LD [HL+], A
+
+void Op_LD_HL_minus_A(CPU& cpu) {}  // LD [HL-], A
+
+// Opcode table initialisation
+std::array<Opcode, 256> opcodeTable = {
+    Opcode{"Nop", 1, 1, Op_Nop},                    // 0x00: No operation
+    Opcode{"LD BC, n16", 3, 3, Op_LD_BC_n16},       // 0x01: Copy the value n16 into register BC.
+    Opcode{"LD [BC], A", 1, 2, Op_LD_BC_A},         // 0x02: Copy the value in register A into the byte pointed to by BC.
+    Opcode{"LD DE, n16", 3, 3, Op_LD_DE_n16},       // 0x11: Copy the value n16 into register DE.
+    Opcode{"LD [DE], A", 1, 2, Op_LD_DE_A},         // 0x12: Copy the value in register A into the byte pointed to by DE.
+    Opcode{"LD HL, n16", 3, 3, Op_LD_HL_n16},       // 0x21: Copy the value n16 into register HL.
+    Opcode{"LD [HL+], A", 1, 2, Op_LD_HL_plus_A},   // 0x22: Copy the value in register A into the byte pointed to by HL+.
+    Opcode{"LD HL, n16", 3, 3, Op_LD_SP_n16},       // 0x31: Copy the value n16 into register SP.
+    Opcode{"LD [HL-], A", 1, 2, Op_LD_HL_minus_A},  // 0x32: Copy the value in register A into the byte pointed to by HL-.
+    // TODO: Add missing instructions
+};
---
+++ src/core/opcodes.h
@@ -0,0 +1,21 @@
+#ifndef __OPCODES_H__
+#define __OPCODES_H__
+
+#include <array>
+#include <string>
+
+#include "cpu.h"
+
+using OpcodeFunc = void (*)(CPU&);  // Callback, takes CPU reference and return void
+
+struct Opcode {
+    std::string name;
+    uint8_t size;
+    uint8_t cycles;
+    OpcodeFunc Execute;
+};
+
+// Declaration of the opcodes table
+extern std::array<Opcode, 256> opcodeTable;
+
+#endif  // __OPCODES_H__
