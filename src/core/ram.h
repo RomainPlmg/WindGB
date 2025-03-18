9f2230adf46b666857afc07fdd1e9650fcc36c4f
Romain Plumaugat <romain.plumaugat@protonmail.com>
2025-03-18 23:42:21 +0100

    Hardening the ram size

--- src/core/ram.h
+++ src/core/ram.h
@@ -4,9 +4,6 @@
 #include <array>
 #include <cstdint>
 
-constexpr int MEMORY_BUS_WIDTH = 16;
-constexpr size_t MEMORY_SIZE = static_cast<size_t>(1) << MEMORY_BUS_WIDTH;
-
 class RAM {
    public:
     RAM();
@@ -14,7 +11,7 @@ class RAM {
     void Write(uint16_t addr, uint8_t data);
 
    private:
-    std::array<uint8_t, MEMORY_SIZE> m_Memory;
+    std::array<uint8_t, 655336> m_Memory;
 };
 
 #endif  // __RAM_H__
