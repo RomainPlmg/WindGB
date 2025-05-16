37fd07f21a61871842be7dbf9f5ddbd0bfdd5e6a
Romain Plumaugat <romain.plumaugat@protonmail.com>
2025-05-16 23:50:52 +0200

    Update method for tile pixels

--- emu/src/tile.cpp
+++ emu/src/tile.cpp
@@ -1,28 +1,30 @@
 #include "tile.h"
 
-Tile::Tile(u8* baseAddr) : m_Data(baseAddr) {}
+static constexpr std::array<std::array<u8, 4>, 4> GB_COLORS = {{
+    {8, 24, 32, 255},      // colorID = 0
+    {52, 104, 86, 255},    // colorID = 1
+    {136, 192, 112, 255},  // colorID = 2
+    {224, 248, 208, 255}   // colorID = 3
+}};
 
-std::array<u8, TILE_WIDTH * TILE_WIDTH * 4> Tile::GetPixels() const {
-    std::array<u8, TILE_WIDTH * TILE_WIDTH * 4> pixels;
+Tile::Tile(const u8* baseAddr) : m_Data(baseAddr) {}
 
+void Tile::Update() {
     for (u8 row = 0; row < TILE_WIDTH; row++) {
         u8 firstByte = *(m_Data + row * 2);
         u8 secondByte = *(m_Data + row * 2 + 1);
-
         for (u8 column = 0; column < TILE_WIDTH; column++) {
+            size_t pixelIndex = (row * TILE_WIDTH + column) * 4;
             u8 offset = 1 << (7 - column);
-            u8 colorID = (firstByte & offset) << 1 | (secondByte & offset);
+            u8 lo = (firstByte & offset) ? 1 : 0;
+            u8 hi = (secondByte & offset) ? 1 : 0;
+            u8 colorID = (hi << 1) | lo;
 
-            switch (colorID) {
-                case 0:
-
-                    break;
-
-                default:
-                    break;
-            }
+            const auto& color = GB_COLORS[colorID];
+            m_Pixels[pixelIndex + 0] = color[0];
+            m_Pixels[pixelIndex + 1] = color[1];
+            m_Pixels[pixelIndex + 2] = color[2];
+            m_Pixels[pixelIndex + 3] = color[3];
         }
     }
-
-    return pixels;
 }
--- emu/src/tile.h
+++ emu/src/tile.h
@@ -4,14 +4,17 @@
 
 #include "common.h"
 
-constexpr u8 TILE_SIZE = 16; // Nb bytes for a tile
-constexpr u8 TILE_WIDTH = 8; // Width of the tile in term of pixels
+constexpr u8 TILE_SIZE = 16;  // Nb bytes for a tile
+constexpr u8 TILE_WIDTH = 8;  // Width of the tile in term of pixels
 
 // For more info: https://gbdev.io/pandocs/Tile_Data.html
 class Tile {
    public:
-    Tile(u8* baseAddr);
-    std::array<u8, TILE_WIDTH * TILE_WIDTH * 4> GetPixels() const;
+    Tile(const u8* baseAddr);
+    void Update();
+    const u8* GetPixels() const { return m_Pixels.data(); }
+
    private:
-    u8* m_Data;
+    const u8* m_Data;
+    std::array<u8, TILE_WIDTH * TILE_WIDTH * 4> m_Pixels;
 };
