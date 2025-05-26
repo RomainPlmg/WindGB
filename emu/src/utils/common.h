8e9a902f0b04d28c8131ad4d815be2d5da4e6187
--global <--global>
2025-05-26 18:50:44 +0200

    Patch priority on GET_BIT expression

--- emu/src/utils/common.h
+++ emu/src/utils/common.h
@@ -8,6 +8,6 @@ using u16 = uint16_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
 
-constexpr bool GET_BIT(u32 a, u8 n) { return (a & (1 << n) != 0); }
+constexpr bool GET_BIT(u32 a, u8 n) { return ((a & (1 << n)) != 0); }
 constexpr void SET_BIT(u32 a, u8 n, bool val) { val ? a |= (1 << n) : a &= ~(1 << n); }
 void NO_IMPL();
